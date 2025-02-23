73b667d48719062fc4fc248f7e98fc55d658b14d
Михаил <kamaltdinova7566@gmail.com>
2025-02-23 13:54:03 +0300

    rased database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 
 import {Button} from "@/shared/ui/button";
+import {prisma} from "@/shared/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany()
+  console.log(games)
   return (
       <><Button size="lg">SSSSSS</Button></>
   );
