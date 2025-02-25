99a84b2236cd886c48e7ca1b9fde77789a541334
Михаил <kamaltdinova7566@gmail.com>
2025-02-25 14:43:24 +0300

    prettier format done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
-
-import {prisma} from "@/shared/lib/db";
-import {Card, CardTitle} from "@/shared/ui/card";
+import { prisma } from "@/shared/lib/db";
+import { Card, CardTitle } from "@/shared/ui/card";
 
 export default async function Home() {
-  const games = await prisma.game.findMany()
-    console.log(games)
+  const games = await prisma.game.findMany();
+  console.log(games);
   return (
-      <>
-          {games.map(game => { return (
-              <Card key = {game.id}>
-                <CardTitle>
-                    {game.name}
-                </CardTitle>
-              </Card>)
-          })}
-      </>
+    <>
+      {games.map((game) => {
+        return (
+          <Card key={game.id}>
+            <CardTitle>{game.name}</CardTitle>
+          </Card>
+        );
+      })}
+    </>
   );
 }
